1502ed606e94d48a5731c9bd73758a615f61faf9
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 13:53:44 +0530

    desktop mapper

--- src/main/java/com/asl/itinventory/mapper/DesktopMapper.java
+++ src/main/java/com/asl/itinventory/mapper/DesktopMapper.java
@@ -1,4 +1,7 @@
 package com.asl.itinventory.mapper;
 
+@Mapper
 public interface DesktopMapper {
+  DesktopEntity mapDesktopEntityFromModel(DesktopModel desktopModel);
+  List<DesktopModel> mapDesktopModelsFromEntities(List<DesktopEntity> desktopEntityList);
 }
