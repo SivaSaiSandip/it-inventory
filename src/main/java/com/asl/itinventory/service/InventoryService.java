a17e572236233153eaa28e60fd658819ada8bf2d
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 15:37:08 +0530

    service methods

--- src/main/java/com/asl/itinventory/service/InventoryService.java
+++ src/main/java/com/asl/itinventory/service/InventoryService.java
@@ -1,4 +1,10 @@
 package com.asl.itinventory.service;
 
+import java.util.List;
+
 public interface InventoryService {
+  
+  void saveDesktopEntry(DesktopModel desktopModel);
+  
+  List<DesktopModel> getAllDesktopEntries();
 }
