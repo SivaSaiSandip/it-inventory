50556890f91f3f732f8807295af2d11f01069863
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 15:34:59 +0530

    dao impl

--- src/main/java/com/asl/itinventory/repository/impl/InventoryDaoImpl.java
+++ src/main/java/com/asl/itinventory/repository/impl/InventoryDaoImpl.java
@@ -1,4 +1,17 @@
 package com.asl.itinventory.repository.impl;
 
-public class InventoryDaoImpl {
+@Service
+@Slf4j
+public class InventoryDaoImpl implements InventoryDao{
+  
+  @Autowired
+  DesktopRepository desktopRepository;
+  
+  void saveDesktopEntity(DesktopEntity desktopEntity){
+    desktopRepository.save(desktopEntity);
+  }
+  
+  List<DesktopEntity> getAllDesktopEntities(){
+    return desktopRepository.fetchAll();
+  }
 }
