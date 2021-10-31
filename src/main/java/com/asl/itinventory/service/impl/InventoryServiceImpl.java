db04cda6326f72bb3f20a169cbfebd4e1f236729
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 15:44:42 +0530

    service implemented

--- src/main/java/com/asl/itinventory/service/impl/InventoryServiceImpl.java
+++ src/main/java/com/asl/itinventory/service/impl/InventoryServiceImpl.java
@@ -1,6 +1,22 @@
 package com.asl.itinventory.service.impl;
 
-public class InventoryServiceImpl {
-
-
-        }
+@Service
+@Slf4j
+public class InventoryServiceImpl implements InventoryService{
+  
+  @Autowired
+  DesktopMapper desktopMapper;
+  @Autowired
+  InventoryDao inventoryDao;
+        
+  @Override        
+  void saveDesktopEntry(DesktopModel desktopModel){
+      DesktopEntity desktopEntity = desktopMapper.mapDesktopEntityFromModel(desktopModel);
+      inventoryDao.saveDesktopEntity(desktopEntity);
+  }
+  @Override        
+  List<DesktopModel> getAllDesktopEntries(){
+      List<DesktopEntity> desktopEntityList = inventoryDao.getAllDesktopEntities();
+      return desktopMapper.mapDesktopModelsFromEntities(desktopEntityList);
+  }
+}
