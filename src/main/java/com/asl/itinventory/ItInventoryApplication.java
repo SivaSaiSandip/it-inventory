5c60d9685147637caa27a6283216e72427a40369
SivaSaiSandip <learninglevinho@gmail.com>
2021-10-31 11:19:42 +0530

    Initial commit

---
+++ src/main/java/com/asl/itinventory/ItInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.asl.itinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asl/itinventory/ItInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asl.itinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
