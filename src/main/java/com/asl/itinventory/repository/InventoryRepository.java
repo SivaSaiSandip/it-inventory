02247fde77138a9b1005499681f52b4a607c26b4
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 14:34:12 +0530

    repo

--- src/main/java/com/asl/itinventory/repository/InventoryRepository.java
+++ src/main/java/com/asl/itinventory/repository/InventoryRepository.java
@@ -1,4 +1,9 @@
 package com.asl.itinventory.repository;
 
-public interface InventoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Desktop, Long>{
+
 }
