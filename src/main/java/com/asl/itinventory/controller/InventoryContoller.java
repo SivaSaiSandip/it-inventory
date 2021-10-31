bf9c5b9b42073b42788a5a18169225b3ef1a604e
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 15:54:07 +0530

    controller

--- src/main/java/com/asl/itinventory/controller/InventoryContoller.java
+++ src/main/java/com/asl/itinventory/controller/InventoryContoller.java
@@ -1,5 +1,23 @@
 package com.asl.itinventory.controller;
 
+@RestController
 public class InventoryContoller {
+  
+  @Autowired
+  InventoryService inventoryService;
+  
+  @PostMapping("/save-desktop-details")
+    public ResponseEntity<Object> saveDesktopDetails(@RequestBody DesktopModel desktopModel){
+      
+        inventoryService.saveStudentDetails(workbook);
+        return new ResponseEntity<Object>("Success", HttpStatus.OK);
+    }
+  
+  @GetMapping("/get-desktop-details")
+    public ResponseEntity<Object> getDesktopDetails() {
+      
+        List<DesktopModel> desktopModelList  = inventoryService.getAllDesktopEntries();
+        return new ResponseEntity<Object>(desktopModelList, HttpStatus.OK);
+    }
 
 }
