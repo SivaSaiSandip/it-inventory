8a3d46bf4aa23214684b20e880fbe40e007ff49a
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 13:46:39 +0530

    laptop model

--- src/main/java/com/asl/itinventory/model/LaptopModel.java
+++ src/main/java/com/asl/itinventory/model/LaptopModel.java
@@ -1,4 +1,19 @@
 package com.asl.itinventory.model;
 
+@Data
 public class LaptopModel {
+    private Long laptopId;
+    private String userName;
+    private String sfid;
+    private Long laptopSno;
+    private String model;
+    private String designation;
+    private String mobile;
+    private String officeMobile;
+    private String division;
+    private Date purchasedOn;
+    private Date warrantyUpto;
+    private String itemCode;
+    private String price;
+    private String remark;
 }
