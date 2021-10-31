51f025622e7f4944c00bc55ed6b7cd1761aafb99
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 13:44:33 +0530

    printer entity

--- src/main/java/com/asl/itinventory/entity/PrinterEntity.java
+++ src/main/java/com/asl/itinventory/entity/PrinterEntity.java
@@ -1,4 +1,23 @@
 package com.asl.itinventory.entity;
-
+@Data
+@Entity
+@Table(name = "printer", schema = "inventory")
 public class PrinterEntity {
+  @Id
+    @GeneratedValue(generator = "printer_seq")
+    @SequenceGenerator(name = "printer_seq",schema = "inventory", sequenceName = "printer_seq", initialValue = 1, allocationSize = 1)
+    private Long printerId;
+    private String userName;
+    private String sfid;
+    private Long printerSno;
+    private String model;
+    private String designation;
+    private String mobile;
+    private String officeMobile;
+    private String division;
+    private Date purchasedOn;
+    private Date warrantyUpto;
+    private String itemCode;
+    private String price;
+    private String remark;
 }
