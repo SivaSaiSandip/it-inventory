9f711faf0733ff98f48703fa82fb3e2789878232
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 14:28:36 +0530

    laptop mapper

--- src/main/java/com/asl/itinventory/mapper/LaptopMapper.java
+++ src/main/java/com/asl/itinventory/mapper/LaptopMapper.java
@@ -1,4 +1,10 @@
 package com.asl.itinventory.mapper;
 
+import org.mapstruct.Mapper;
+import java.util.List;
+
+@Mapper
 public interface LaptopMapper {
+  LaptopEntity mapLaptopEntityFromModel(LaptopModel laptopModel);
+  List<LaptopModel> mapLaptopModelsFromEntities(List<LaptopEntity> laptopEntityList);
 }
