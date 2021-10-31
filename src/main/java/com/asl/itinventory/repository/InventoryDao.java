f5844ba1e484a2301e567d2cc902673bb21d7f03
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 14:49:00 +0530

    desktop calls in inventory

--- src/main/java/com/asl/itinventory/repository/InventoryDao.java
+++ src/main/java/com/asl/itinventory/repository/InventoryDao.java
@@ -1,4 +1,11 @@
 package com.asl.itinventory.repository;
 
+import java.util.List;
+
 public interface InventoryDao {
+  
+  void saveDesktopEntity(DesktopEntity desktopEntity);
+  
+  List<DesktopEntity> getAllDesktopEntities();
+  
 }
