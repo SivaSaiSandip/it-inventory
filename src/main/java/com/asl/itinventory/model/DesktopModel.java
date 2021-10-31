29f6bf0623b0c6165b8940f8d7a51e01128930dc
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 13:45:36 +0530

    desktop model

--- src/main/java/com/asl/itinventory/model/DesktopModel.java
+++ src/main/java/com/asl/itinventory/model/DesktopModel.java
@@ -1,4 +1,19 @@
 package com.asl.itinventory.model;
 
+@Data
 public class DesktopModel {
+    private Long desktopId;
+    private String userName;
+    private String sfid;
+    private Long desktopSno;
+    private String model;
+    private String designation;
+    private String mobile;
+    private String officeMobile;
+    private String division;
+    private Date purchasedOn;
+    private Date warrantyUpto;
+    private String itemCode;
+    private String price;
+    private String remark;
 }
