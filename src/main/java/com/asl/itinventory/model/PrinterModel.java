fe1c209b3f20980018134bc306e9ea0a45165754
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 13:47:42 +0530

    printer model

--- src/main/java/com/asl/itinventory/model/PrinterModel.java
+++ src/main/java/com/asl/itinventory/model/PrinterModel.java
@@ -1,4 +1,19 @@
 package com.asl.itinventory.model;
 
+@Data
 public class PrinterModel {
+    private Long printerId;
+    private String userName;
+    private String sfid;
+    private Long printerSno;
+    private String model;
+    private String designation;
+    private String mobile;
+    private String officeMobile;
+    private String division;
+    private Date purchasedOn;
+    private Date warrantyUpto;
+    private String itemCode;
+    private String price;
+    private String remark;
 }
