50763d9e491b4853c6ed61ae17c0825c07cf349c
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 13:43:08 +0530

    Update LaptopEntity.java
    laptop entity

--- src/main/java/com/asl/itinventory/entity/LaptopEntity.java
+++ src/main/java/com/asl/itinventory/entity/LaptopEntity.java
@@ -1,4 +1,23 @@
 package com.asl.itinventory.entity;
-
+@Data
+@Entity
+@Table(name = "laptop", schema = "inventory")
 public class LaptopEntity {
+  @Id
+    @GeneratedValue(generator = "laptop_seq")
+    @SequenceGenerator(name = "laptop_seq",schema = "inventory", sequenceName = "laptop_seq", initialValue = 1, allocationSize = 1)
+    private Long laptopId;
+    private String userName;
+    private String sfid;
+    private Long laptopSno;
+    private String model;
+    private String designation;
+    private String mobile;
+    private String officeMobile;
+    private String division;
+    private Date purchasedOn;
+    private Date warrantyUpto;
+    private String itemCode;
+    private String price;
+    private String remark;
 }
