58be8cab986192dc4d67d954e39791faaa294369
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 14:31:46 +0530

    printer mapper

--- src/main/java/com/asl/itinventory/mapper/PrinterMapper.java
+++ src/main/java/com/asl/itinventory/mapper/PrinterMapper.java
@@ -1,4 +1,10 @@
 package com.asl.itinventory.mapper;
 
+import org.mapstruct.Mapper;
+import java.util.List;
+
+@Mapper
 public interface PrinterMapper {
+  PrinterEntity mapPrinterEntityFromModel(PrinterModel printerModel);
+  List<PrinterModel> mapPrinterModelsFromEntities(List<PrinterEntity> printerEntityList);
 }
