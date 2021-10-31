2f8bf7bda3c286e1243d2e1bc1360e1cf083413d
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-10-31 13:41:31 +0530

    Update DesktopEntity.java
    desktop entity created

--- src/main/java/com/asl/itinventory/entity/DesktopEntity.java
+++ src/main/java/com/asl/itinventory/entity/DesktopEntity.java
@@ -1,4 +1,23 @@
 package com.asl.itinventory.entity;
-
+@Data
+@Entity
+@Table(name = "desktop", schema = "inventory")
 public class DesktopEntity {
+    @Id
+    @GeneratedValue(generator = "desktop_seq")
+    @SequenceGenerator(name = "desktop_seq",schema = "inventory", sequenceName = "desktop_seq", initialValue = 1, allocationSize = 1)
+    private Long desktopId;
+    private String userName;
+    private String sfid;
+    private Long desktopSno;
+    private String model;
+    private String designation;
+    private String mobile;
+    private String officeMobile;
+    private String division;
+    private Date purchasedOn;
+    private Date warrantyUpto;
+    private String itemCode;
+    private String price;
+    private String remark;
 }
