3a3a233a1301ef05838b1da7878c44065b8af7af
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-11-02 21:28:07 +0530

    added methods in service

--- src/main/java/com/asl/itinventory/service/InventoryService.java
+++ src/main/java/com/asl/itinventory/service/InventoryService.java
@@ -1,6 +1,8 @@
 package com.asl.itinventory.service;
 
 import com.asl.itinventory.model.DesktopModel;
+import com.asl.itinventory.model.PrinterModel;
+import com.asl.itinventory.model.LaptopModel;
 
 import java.util.List;
 
@@ -9,4 +11,12 @@ public interface InventoryService {
   void saveDesktopEntry(DesktopModel desktopModel);
   
   List<DesktopModel> getAllDesktopEntries();
+  
+  void savePrinterEntry(PrinterModel printerModel);
+  
+  List<PrinterModel> getAllPrinterEntries();
+  
+  void saveLaptopEntry(LaptopModel laptopModel);
+  
+  List<LaptopModel> getAllLaptopEntries();
 }
