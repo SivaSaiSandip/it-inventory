e087ddf52a31eb8a11dd6ebccb4cb16e6f196ecd
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-11-02 21:22:51 +0530

    more methods in dao impl

--- src/main/java/com/asl/itinventory/repository/impl/InventoryDaoImpl.java
+++ src/main/java/com/asl/itinventory/repository/impl/InventoryDaoImpl.java
@@ -2,6 +2,8 @@ package com.asl.itinventory.repository.impl;
 
 import com.asl.itinventory.entity.DesktopEntity;
 import com.asl.itinventory.repository.DesktopRepository;
+import com.asl.itinventory.repository.PrinterRepository;
+import com.asl.itinventory.repository.LaptopRepository;
 import com.asl.itinventory.repository.InventoryDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +17,29 @@ public class InventoryDaoImpl implements InventoryDao {
   
   @Autowired
   DesktopRepository desktopRepository;
+   @Autowired
+  PrinterRepository printerRepository;
+   @Autowired
+  LaptopRepository laptopRepository;
+  
   @Override
  public void saveDesktopEntity(DesktopEntity desktopEntity){desktopRepository.save(desktopEntity);}
  @Override
   public List<DesktopEntity> getAllDesktopEntities(){
     return desktopRepository.findAll();
+  }
+  
+ @Override
+ public void savePrinterEntity(PrinterEntity printerEntity){printerRepository.save(printerEntity);}
+ @Override
+  public List<PrinterEntity> getAllPrinterEntities(){
+    return printerRepository.findAll();
+  }
+  
+ @Override
+ public void saveLaptopEntity(LaptopEntity laptopEntity){laptopRepository.save(laptopEntity);}
+ @Override
+  public List<LaptopEntity> getAllLaptopEntities(){
+    return laptopRepository.findAll();
   }
 }
