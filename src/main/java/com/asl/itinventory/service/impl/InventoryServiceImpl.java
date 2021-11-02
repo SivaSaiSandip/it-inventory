e5fc9f744fc7184e37d40992fdc0398be80d5341
BunnyBear95 <91249069+BunnyBear95@users.noreply.github.com>
2021-11-02 21:40:37 +0530

    service changes

--- src/main/java/com/asl/itinventory/service/impl/InventoryServiceImpl.java
+++ src/main/java/com/asl/itinventory/service/impl/InventoryServiceImpl.java
@@ -1,8 +1,14 @@
 package com.asl.itinventory.service.impl;
 
 import com.asl.itinventory.entity.DesktopEntity;
+import com.asl.itinventory.entity.PrinterEntity;
+import com.asl.itinventory.entity.LaptopEntity;
 import com.asl.itinventory.mapper.DesktopMapper;
+import com.asl.itinventory.mapper.PrinterMapper;
+import com.asl.itinventory.mapper.LaptopMapper;
 import com.asl.itinventory.model.DesktopModel;
+import com.asl.itinventory.model.LaptopModel;
+import com.asl.itinventory.model.PrinterModel;
 import com.asl.itinventory.repository.InventoryDao;
 import com.asl.itinventory.service.InventoryService;
 import lombok.extern.slf4j.Slf4j;
@@ -19,12 +25,14 @@ public class InventoryServiceImpl implements InventoryService {
   @Autowired
   DesktopMapper desktopMapper;
   @Autowired
+  PrinterMapper printerMapper;
+  @Autowired
+  LaptopMapper laptopMapper;
+  @Autowired
   InventoryDao inventoryDao;
         
   @Override        
   public void saveDesktopEntry(DesktopModel desktopModel){
-   //   DesktopEntity desktopEntity = new DesktopEntity();
-     //         BeanUtils.copyProperties(desktopEntity,desktopModel);
      DesktopEntity desktopEntity = desktopMapper.mapDesktopEntityFromModel(desktopModel);
       inventoryDao.saveDesktopEntity(desktopEntity);
   }
@@ -32,5 +40,27 @@ public class InventoryServiceImpl implements InventoryService {
   public List<DesktopModel> getAllDesktopEntries(){
       List<DesktopEntity> desktopEntityList = inventoryDao.getAllDesktopEntities();
       return desktopMapper.mapDesktopModelsFromEntities(desktopEntityList);
+  }
+  
+   @Override        
+  public void savePrinterEntry(PrinterModel printerModel){
+     PrinterEntity printerEntity = printerMapper.mapDesktopEntityFromModel(printerModel);
+      inventoryDao.savePrinterEntity(printerEntity);
+  }
+  @Override
+  public List<PrinterModel> getAllPrinterEntries(){
+      List<PrinterEntity> printerEntityList = inventoryDao.getAllPrinterEntities();
+      return printerMapper.mapDesktopModelsFromEntities(printerEntityList);
+  }
+  
+  @Override        
+  public void saveLaptopEntry(LaptopModel laptopModel){
+     LaptopEntity laptopEntity = laptopMapper.mapDesktopEntityFromModel(laptopModel);
+      inventoryDao.saveLaptopEntity(laptopEntity);
+  }
+  @Override
+  public List<LaptopModel> getAllLaptopEntries(){
+      List<LaptopEntity> laptopEntityList = inventoryDao.getAllLaptopEntities();
+      return laptopMapper.mapDesktopModelsFromEntities(laptopEntityList);
   }
 }
